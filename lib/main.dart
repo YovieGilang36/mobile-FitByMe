4d6319bf5b16586d52f6a61f01c1fbfb01cecb09
afinnaufal <afinnaufal.an@gmail.com>
2023-01-01 00:04:12 +0700

    Berhasil Login

---
+++ lib/main.dart
@@ -0,0 +1,328 @@
+// import 'package:fitbyme/navbar.dart';
+import 'package:flutter/material.dart';
+import 'package:fitbyme/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FitByMe',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    );
+  }
+}
+
+// class Login extends StatefulWidget {
+//   @override
+//   State<Login> createState() => _LoginState();
+// }
+
+// class _LoginState extends State<Login> {
+//   //For LinearProgressIndicator.
+//   bool _visible = false;
+
+//   //Textediting Controller for Username and Password Input
+//   TextEditingController email = TextEditingController();
+//   TextEditingController pass = TextEditingController();
+
+//   Future userLogin() async {
+//     //Login API URL
+//     //use your local IP address instead of localhost or use Web API
+//     String url = "http://192.168.43.205/Web-fitbyme/API/login.php";
+
+//     // Showing LinearProgressIndicator.
+//     setState(() {
+//       _visible = true;
+//     });
+
+//     // Getting username and password from Controller
+//     var data = {
+//       'email': email.text,
+//       'password': pass.text,
+//     };
+
+//     //Starting Web API Call.
+//     var response = await http.post(Uri.parse(url), body: json.encode(data));
+//     if (response.statusCode == 200) {
+//       //Server response into variable
+//       print(response.body);
+//       var msg = jsonDecode(response.body);
+
+//       // Check Login Status
+//       if (msg['loginStatus'] == true) {
+
+//         SharedPreferences preferences = await SharedPreferences.getInstance();
+//         preferences.setString('email', email.text);
+
+//         Fluttertoast.showToast(
+//         msg: "Login Successfully",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.CENTER,
+//         timeInSecForIosWeb: 1,
+//         backgroundColor: Colors.green,
+//         textColor: Colors.white,
+//         fontSize: 16.0
+//     );
+//         setState(() {
+//           //hide progress indicator
+//           _visible = false;
+//         });
+
+//         // Navigate to Home Screen
+//         Navigator.push(
+//             context, MaterialPageRoute(builder: (context) => Navbar()));
+//       } else {
+//         Fluttertoast.showToast(
+//         msg: "Email or Password Incorrect",
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.CENTER,
+//         timeInSecForIosWeb: 1,
+//         backgroundColor: Colors.red,
+//         textColor: Colors.white,
+//         fontSize: 16.0
+//     );
+//         setState(() {
+//           //hide progress indicator
+//           _visible = false;
+//         });
+//       }
+//     } else {
+//       setState(() {
+//         //hide progress indicator
+//         _visible = false;
+//       });
+//     }
+//   }
+
+//   final _formKey = GlobalKey<FormState>();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//         child: Scaffold(
+//       body: SingleChildScrollView(
+//         scrollDirection: Axis.vertical,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Visibility(
+//               visible: _visible,
+//               child: Container(
+//                 margin: EdgeInsets.only(bottom: 10.0),
+//                 child: LinearProgressIndicator(),
+//               ),
+//             ),
+//             Container(
+//               height: 50.0,
+//             ),
+//             SizedBox(
+//               height: 10.0,
+//             ),
+//             Text(
+//               "FitByMe APP",
+//               style: TextStyle(
+//                   color: Colors.black,
+//                   fontSize: 20.0,
+//                   fontWeight: FontWeight.bold),
+//             ),
+//             SizedBox(
+//               height: 20.0,
+//             ),
+//             Container(
+//               alignment: Alignment.center,
+//               height: 150,
+//               child: Image.asset(
+//                 "image/login-vektor.png",
+//                 fit: BoxFit.contain,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20.0,
+//             ),
+//             Form(
+//               key: _formKey,
+//               child: Padding(
+//                 padding: const EdgeInsets.all(20.0),
+//                 child: Column(
+//                   children: [
+//                     Theme(
+//                       data: new ThemeData(
+//                         primaryColor: Color.fromRGBO(84, 87, 90, 0.5),
+//                         primaryColorDark: Color.fromRGBO(84, 87, 90, 0.5),
+//                         hintColor:
+//                             Color.fromRGBO(84, 87, 90, 0.5), //placeholder color
+//                       ),
+//                       child: TextFormField(
+//                         controller: email,
+//                         decoration: InputDecoration(
+//                           focusedBorder: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Color.fromRGBO(84, 87, 90, 0.5),
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                           enabledBorder: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Color.fromRGBO(84, 87, 90, 0.5),
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                           errorBorder: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Colors.red,
+//                               width: 1.0,
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                           labelText: 'Email',
+//                           prefixIcon: const Icon(
+//                             Icons.email_outlined,
+//                             color: Color.fromRGBO(84, 87, 90, 0.5),
+//                           ),
+//                           border: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Color.fromRGBO(84, 87, 90, 0.5),
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                         ),
+//                         validator: (value) {
+//                           if (value == null || value.isEmpty) {
+//                             return 'Please Enter Email';
+//                           }
+//                           return null;
+//                         },
+//                       ),
+//                     ),
+//                     SizedBox(
+//                       height: 20.0,
+//                     ),
+//                     Theme(
+//                       data: new ThemeData(
+//                         primaryColor: Color.fromRGBO(84, 87, 90, 0.5),
+//                         primaryColorDark: Color.fromRGBO(84, 87, 90, 0.5),
+//                         hintColor:
+//                             Color.fromRGBO(84, 87, 90, 0.5), //placeholder color
+//                       ),
+//                       child: TextFormField(
+//                         controller: pass,
+//                         obscureText: true,
+//                         decoration: InputDecoration(
+//                           focusedBorder: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Color.fromRGBO(84, 87, 90, 0.5),
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                           enabledBorder: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Color.fromRGBO(84, 87, 90, 0.5),
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                           errorBorder: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Colors.red,
+//                               width: 1.0,
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                           border: new OutlineInputBorder(
+//                             borderSide: BorderSide(
+//                               color: Color.fromRGBO(84, 87, 90, 0.5),
+//                               style: BorderStyle.solid,
+//                             ),
+//                           ),
+//                           labelText: 'Password',
+//                           prefixIcon: const Icon(
+//                             Icons.lock,
+//                             color: Color.fromRGBO(84, 87, 90, 0.5),
+//                           ),
+//                         ),
+//                         validator: (value) {
+//                           if (value == null || value.isEmpty) {
+//                             return 'Please Enter Password';
+//                           }
+//                           return null;
+//                         },
+//                       ),
+//                     ),
+//                     SizedBox(
+//                       height: 20.0,
+//                     ),
+//                     Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: ElevatedButton(
+//                         onPressed: () => {
+//                           // Validate returns true if the form is valid, or false otherwise.
+//                           if (_formKey.currentState!.validate()) {userLogin()}
+//                         },
+//                         child: Padding(
+//                           padding: EdgeInsets.all(10.0),
+//                           child: Text(
+//                             'Sign In',
+//                             style: TextStyle(fontSize: 18.0),
+//                           ),
+//                         ),
+//                         style: ButtonStyle(
+//                           fixedSize: MaterialStateProperty.all(
+//                             Size(250, 50),
+//                           ),
+//                           backgroundColor: MaterialStateProperty.all<Color>(
+//                               Theme.of(context).primaryColor),
+//                           shape: MaterialStateProperty.all(
+//                             RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(30),
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(height: 30),
+//                     Center( 
+//                         child: RichText(
+//                       text: TextSpan(
+//                         text: "Don't have any account? ",
+//                         style: TextStyle(
+//                           color: Colors.black,
+//                         ),
+//                         children: [
+//                           TextSpan(
+//                             recognizer: TapGestureRecognizer()
+//                               ..onTap = () {
+//                                 Navigator.push(
+//                                   context,
+//                                   MaterialPageRoute(
+//                                     builder: (context) => Register(),
+//                                   ),
+//                                 );
+//                               },
+//                             text: "Sign Up",
+//                             style: TextStyle(
+//                               color: Colors.blue,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     )),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     ));
+//   }
+// }
+
